40b383f3900d4a1db028db848ee4985827f95e0e
Lion_Pro <antoni13@email.com>
2017-03-29 12:14:18 +0200

    detect the mouth and lip

--- FaceDetect_Landmark.py
+++ FaceDetect_Landmark.py
@@ -41,14 +41,23 @@ import dlib
 PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
 SCALE_FACTOR = 1
 
+ALL_POINTS = list(range(0, 68))
 FACE_POINTS = list(range(17, 68))
-MOUTH_POINTS = list(range(48, 68))
+MOUTH_POINTS = list(range(48, 60))
+LIP_POINTS = list(range(60, 68))
 RIGHT_BROW_POINTS = list(range(17, 22))
 LEFT_BROW_POINTS = list(range(22, 27))
 RIGHT_EYE_POINTS = list(range(36, 42))
 LEFT_EYE_POINTS = list(range(42, 48))
 NOSE_POINTS = list(range(27, 35))
 JAW_POINTS = list(range(0, 17))
+
+color_red = (0, 0, 255)
+color_blue = (255, 0, 0)
+color_white = (255, 255, 255)
+color_black = (0, 0, 0)
+
+yawning_limit = 0.35
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(PREDICTOR_PATH)
@@ -81,19 +90,31 @@ def read_im_and_landmarks(im):
 
 if __name__ == '__main__':
 
-    color_point = (255, 0, 0)
     cap = cv2.VideoCapture(0)
 
     while cap.isOpened():
         ret, im2 = cap.read()
         landmarks2 = read_im_and_landmarks(im2)
 
-        if landmarks2 is not None:
-            # for i in range(landmarks2.__len__()):
-            for i in MOUTH_POINTS:
-                (a, b) = landmarks2[i]
-                cv2.circle(im2, (a, b), 3, color_point, -1)
+        cv2.rectangle(im2, (30, 450), (200, 450), color_black, 54)
+        cv2.rectangle(im2, (30, 450), (200, 450), color_white, 50)
 
+        if landmarks2 is None:
+            alarm_str = "No Detect!"
+        else:
+            # ---------- draw the landmarks data ------------
+            for i in LIP_POINTS:
+                cv2.circle(im2, landmarks2[i], 3, color_blue, -1)
+
+            # ------------- decide the yawning --------------
+            rate_mouth = float(landmarks2[66][1] - landmarks2[62][1]) / (landmarks2[64][0] - landmarks2[60][0])
+            if rate_mouth > yawning_limit:
+                alarm_str = "Yawning!"
+            else:
+                alarm_str = "Ok!"
+
+        # ------------ display the alarm string --------------
+        cv2.putText(im2, alarm_str, (20, 460), cv2.FONT_HERSHEY_DUPLEX, 1, color_red, 2)
         cv2.imshow("Face Swapped", im2)
 
         k = cv2.waitKey(30) & 0xff
